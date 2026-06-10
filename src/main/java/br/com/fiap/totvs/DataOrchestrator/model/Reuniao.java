73b95aa10a24a4ffd6ba4631e885c2d0a9138f90
CarlosSMariano <sanchesmarianocarlos@gmail.com>
2026-06-10 18:14:09 -0300

    feat(model): create record Reuniao to recive the Json by API

--- src/main/java/br/com/fiap/totvs/DataOrchestrator/model/Reuniao.java
+++ src/main/java/br/com/fiap/totvs/DataOrchestrator/model/Reuniao.java
@@ -3,7 +3,7 @@ package br.com.fiap.totvs.DataOrchestrator.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-// Esta classe representa o JSON bruto, que vamos trabalhar
+// Esta classe representa o JSON bruto
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record Reuniao(

3d96731708ba397d0f43c4430b2edc3838581283
CarlosSMariano <sanchesmarianocarlos@gmail.com>
2026-06-10 17:20:11 -0300

    feat: adding DataOrchestratorApplication

---
+++ src/main/java/br/com/fiap/totvs/DataOrchestrator/DataOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.totvs.DataOrchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataOrchestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataOrchestratorApplication.class, args);
+	}
+
+}

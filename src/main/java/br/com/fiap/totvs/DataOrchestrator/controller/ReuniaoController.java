da4ff44b4620c4854bbb829f0bfdff167de90d20
CarlosSMariano <sanchesmarianocarlos@gmail.com>
2026-06-11 18:29:38 -0300

    feat(controller): create Health Check Endpoint

--- src/main/java/br/com/fiap/totvs/DataOrchestrator/controller/ReuniaoController.java
+++ src/main/java/br/com/fiap/totvs/DataOrchestrator/controller/ReuniaoController.java
@@ -6,15 +6,25 @@ import br.com.fiap.totvs.DataOrchestrator.service.AnaliseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
-@RequestMapping("/reunioes")
+@RequestMapping("/api/reunioes")
 public class ReuniaoController {
 
     @Autowired
     private AnaliseService service;
 
+    @GetMapping("/status")
+    public Map<String, String> status() {
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "Tudo certo!");
+        response.put("code", "200");
+        response.put("message", "API DataOrchestrator está operando normalmente");
+        return response;
+    }
     @PostMapping
     public ResultadoAnalise inserirReuniao(@RequestBody Reuniao reuniao) {
         return service.processarReuniao(reuniao);

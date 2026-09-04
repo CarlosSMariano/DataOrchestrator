6a955ea82e9d323023ee91b18e0dde7ce66bbeda
CarlosSMariano <sanchesmarianocarlos@gmail.com>
2026-09-04 17:47:38 -0300

    fix: docker compose

--- src/main/java/br/com/fiap/totvs/DataOrchestrator/config/ConexaoComBanco.java
+++ src/main/java/br/com/fiap/totvs/DataOrchestrator/config/ConexaoComBanco.java
@@ -5,11 +5,21 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexaoComBanco {
-    public static Connection getConnection() throws SQLException {
-        String url = "jdbc:oracle:thin:@localhost:1521/FREEPDB1";
-        String user = "admin";
-        String password = "admin";
 
-        return DriverManager.getConnection(url, user, password);
+    private static final String URL = "jdbc:oracle:thin:@localhost:1521/FREEPDB1";
+    private static final String USER = "admin_totvs";
+    private static final String PASSWORD = "admin";
+
+    public static Connection getConnection() throws SQLException {
+
+        try{
+            Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Conexão efetuada com sucesso!");
+            return conn;
+
+        }catch(SQLException e){
+            System.err.println("Erro ao conectar ao banco de dados:" + e.getMessage());
+            throw e;
+        }
     }
 }
